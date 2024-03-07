a05788793ab4b6718e4cc976710d43f1debb25b2
Romeu Lucas <162071864+RomeuL@users.noreply.github.com>
2024-03-06 23:18:37 -0300

    Add files via upload

---
+++ barbecue.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("responderBtn");
+    const condimentoInput = document.getElementById("condimentoInput");
+    const imagemContainer = document.getElementById("imagemContainer");
+
+    button.addEventListener("click", function() {
+        const condimento = condimentoInput.value.trim().toLowerCase();
+        const respostascorretas = ["barbecue", "Barbecue", "BARBECUE"];
+        if (respostascorretas.includes(condimento)) {
+            carregarImagem("barbe.jpg", "barbecuitch.mp3");
+            
+        } else {
+            alert("Resposta errada");
+        }
+    });
+
+    function carregarImagem(path, audioPath) {
+        const container = document.createElement("div");
+        container.classList.add("imagem-container");
+        
+        const imagem = document.createElement("img");
+        imagem.src = path;
+        imagem.width = 300;
+        imagem.height = 450;
+        container.appendChild(imagem);
+    
+        const audio = document.createElement("audio");
+        audio.src = audioPath;
+        audio.controls = true;
+        audio.autoplay = true;
+        container.appendChild(audio);
+    
+        imagemContainer.innerHTML = "";
+        imagemContainer.appendChild(container);
+    }
+});
